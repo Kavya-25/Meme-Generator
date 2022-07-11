ec4afd1832ab23f884977708932dcf21c3bdb962
Kavya-25 <90210074+Kavya-25@users.noreply.github.com>
2022-07-11 16:03:19 +0530

    added random images section

--- src/Components/Inputs/Input.js
+++ src/Components/Inputs/Input.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Input.scss'
+import {memesData} from '../Data/memesData'
 
 export default function Input() {
+  
+  const [memeImage,setmemeImage]=useState();
+
+  const setimageHandler=()=>{
+    const memeimageArray=memesData.data.memes;
+  const randomNumber=Math.floor(Math.random()*memeimageArray.length);
+  console.log(memeimageArray[randomNumber].url);
+    setmemeImage(memeimageArray[randomNumber].url)
+
+  }
   return (
     <div className='input-box'>
-    <form>
     <div className="inputs">
         <input type="text" placeholder='Top text' />
         <input type="text" placeholder='Bottom text'/>
         </div>
-        <button className='inputBtn'>Get a new meme image </button>
-        </form>
+        <button className='inputBtn' onClick={setimageHandler}>Get a new meme image </button>
+        <img src={memeImage} alt="" width="300px"/>
     </div>
 
   )
